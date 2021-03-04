2ac4d962cefb8085dae391524cbfb2148fe9d5d5
MicroProgramer <microprogramer@yahoo.com>
2021-03-04 08:10:14 +0500

    minSDK set to 19 updated to 1.0.2

--- app/src/main/java/com/microprogramer/circularimageview/MainActivity.java
+++ app/src/main/java/com/microprogramer/circularimageview/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.microprogramer.library.CircularImageView;
+import com.squareup.picasso.Picasso;
+
 public class MainActivity extends AppCompatActivity {
 
+    private CircularImageView me_avatar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        me_avatar = findViewById(R.id.me_avatar);
+
+
+        String link = "https://i.guim.co.uk/img/media/c8b1d78883dfbe7cd3f112495941ebc0b25d2265/256_0_3840_2304/master/3840.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=579884b0bd058f1350519d3cc586d587";
+
+        Picasso.get().load(link).into(me_avatar);
+
     }
 }
